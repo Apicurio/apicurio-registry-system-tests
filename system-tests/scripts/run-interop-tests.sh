7fefbb6551e9cd3ccb614778203241e7f09777ec
Radim Kubis <rkubis@redhat.com>
2023-05-05 10:32:13 +0200

    Update(interop): Do not clean Maven

--- system-tests/scripts/run-interop-tests.sh
+++ system-tests/scripts/run-interop-tests.sh
@@ -4,4 +4,4 @@ FORCE_NAMESPACE=interop-test-namespace \
     KAFKA_PACKAGE=amq-streams \
     KAFKA_DEPLOYMENT=amq-streams-cluster-operator \
     CATALOG=redhat-operators \
-    mvn clean test -P interop
+    mvn test -P interop
