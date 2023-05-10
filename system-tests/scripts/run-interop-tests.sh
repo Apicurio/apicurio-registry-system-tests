93dccc0021bebf9cebd47e4819656595d53d571b
Radim Kubis <rkubis@redhat.com>
2023-05-10 13:46:55 +0200

    Update(run-interop-tests): Set Maven local repository path

--- system-tests/scripts/run-interop-tests.sh
+++ system-tests/scripts/run-interop-tests.sh
@@ -4,4 +4,4 @@ FORCE_NAMESPACE=interop-test-namespace \
     KAFKA_PACKAGE=amq-streams \
     KAFKA_DEPLOYMENT=amq-streams-cluster-operator \
     CATALOG=redhat-operators \
-    mvn test -P interop
+    mvn test -P interop -Dmaven.repo.local=/tmp/m2
