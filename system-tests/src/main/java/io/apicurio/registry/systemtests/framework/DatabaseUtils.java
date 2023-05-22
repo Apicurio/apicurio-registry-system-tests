5534b0d5fb9bc9a52623d3cf9a87723d4354792e
Radim Kubis <rkubis@redhat.com>
2023-05-22 11:24:12 +0200

    Update(PostgreSQL): Do not use PVC, possibility to set database version

--- system-tests/src/main/java/io/apicurio/registry/systemtests/framework/DatabaseUtils.java
+++ system-tests/src/main/java/io/apicurio/registry/systemtests/framework/DatabaseUtils.java
@@ -2,20 +2,16 @@ package io.apicurio.registry.systemtests.framework;
 
 import io.apicurio.registry.systemtests.registryinfra.ResourceManager;
 import io.apicurio.registry.systemtests.registryinfra.resources.DeploymentResourceType;
-import io.apicurio.registry.systemtests.registryinfra.resources.PersistentVolumeClaimResourceType;
 import io.apicurio.registry.systemtests.registryinfra.resources.ServiceResourceType;
-import io.fabric8.kubernetes.api.model.PersistentVolumeClaim;
 import io.fabric8.kubernetes.api.model.Service;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
 
 public class DatabaseUtils {
     public static void deployDefaultPostgresqlDatabase() {
-        PersistentVolumeClaim persistentVolumeClaim = PersistentVolumeClaimResourceType.getDefaultPostgresql();
         Deployment deployment = DeploymentResourceType.getDefaultPostgresql();
         Service service = ServiceResourceType.getDefaultPostgresql();
 
         try {
-            ResourceManager.getInstance().createResource(false, persistentVolumeClaim);
             ResourceManager.getInstance().createResource(true, deployment);
             ResourceManager.getInstance().createResource(false, service);
         } catch (Exception e) {
@@ -24,15 +20,10 @@ public class DatabaseUtils {
     }
 
     public static void deployPostgresqlDatabase(String name, String namespace) {
-        PersistentVolumeClaim persistentVolumeClaim = PersistentVolumeClaimResourceType.getDefaultPostgresql(
-                name,
-                namespace
-        );
         Deployment deployment = DeploymentResourceType.getDefaultPostgresql(name, namespace);
         Service service = ServiceResourceType.getDefaultPostgresql(name, namespace);
 
         try {
-            ResourceManager.getInstance().createResource(false, persistentVolumeClaim);
             ResourceManager.getInstance().createResource(true, deployment);
             ResourceManager.getInstance().createResource(false, service);
         } catch (Exception e) {
