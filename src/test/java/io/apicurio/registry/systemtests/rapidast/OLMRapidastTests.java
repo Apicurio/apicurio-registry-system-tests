f8ac25d6a78b2b2dc7582dab81dc47d5f8b3510b
Radim Kubis <rkubis@redhat.com>
2023-12-05 17:25:34 +0100

    Update(*Tests): Set source to null for testing live version

--- src/test/java/io/apicurio/registry/systemtests/rapidast/OLMRapidastTests.java
+++ src/test/java/io/apicurio/registry/systemtests/rapidast/OLMRapidastTests.java
@@ -15,7 +15,7 @@ public abstract class OLMRapidastTests extends RapidastTests {
     public void testBeforeEach(ExtensionContext testContext) throws InterruptedException {
         LOGGER.info("BeforeEach: " + testContext.getTestMethod().get().getName());
 
-        ApicurioRegistryOLMOperatorType registryOLMOperator = new ApicurioRegistryOLMOperatorType(clusterWide);
+        ApicurioRegistryOLMOperatorType registryOLMOperator = new ApicurioRegistryOLMOperatorType(null, clusterWide);
 
         operatorManager.installOperator(registryOLMOperator);
     }
