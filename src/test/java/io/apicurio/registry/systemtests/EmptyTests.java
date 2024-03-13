503b5c4c2566b138a5faf63aa6adf86498c6b5b2
Radim Kubis <rkubis@redhat.com>
2024-03-13 21:35:26 +0100

    New(tests): Empty tests

---
+++ src/test/java/io/apicurio/registry/systemtests/EmptyTests.java
@@ -0,0 +1,12 @@
+package io.apicurio.registry.systemtests;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("empty")
+public class EmptyTests {
+    @Test
+    public void emptyTest() {
+        System.out.println("This is empty test!");
+    }
+}
