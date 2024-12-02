728a71bb1fe8b62b4a3bcf205d7d20f3af348c4c
Radim Kubis <rkubis@redhat.com>
2024-12-02 20:34:18 +0100

    Update(Artifact): Use placeholder and replace for artifact ID

--- src/main/java/io/apicurio/registry/systemtests/client/ArtifactContent.java
+++ src/main/java/io/apicurio/registry/systemtests/client/ArtifactContent.java
@@ -2,10 +2,12 @@ package io.apicurio.registry.systemtests.client;
 
 import java.util.HashMap;
 
+import io.apicurio.registry.systemtests.framework.Constants;
+
 public final class ArtifactContent {
     public static final String DEFAULT_AVRO = "{" +
             "\"artifactType\":\"AVRO\"," +
-            "\"artifactId\":\"<artifact_id>\"," +
+            "\"artifactId\":\"" + Constants.AVRO_ARTIFACT_ID_PLACEHOLDER + "\"," +
             "\"name\":\"price\"," +
             "\"namespace\":\"com.example\"," +
             "\"type\":\"record\"," +
