e088bc5bc391654e24623d2da7fa5f6feecb710d
Radim Kubis <rkubis@redhat.com>
2024-12-02 18:32:23 +0100

    Update(APIclient): ArtifactId is required

--- src/main/java/io/apicurio/registry/systemtests/client/ArtifactContent.java
+++ src/main/java/io/apicurio/registry/systemtests/client/ArtifactContent.java
@@ -3,6 +3,7 @@ package io.apicurio.registry.systemtests.client;
 public final class ArtifactContent {
     public static final String DEFAULT_AVRO = "{" +
             "\"artifactType\":\"AVRO\"," +
+            "\"artifactId\":\"<artifact_id>\"," +
             "\"name\":\"price\"," +
             "\"namespace\":\"com.example\"," +
             "\"type\":\"record\"," +
