4775275dbaaf9aeefff6dc6a4e3f857c9d63a472
Radim Kubis <rkubis@redhat.com>
2024-12-02 16:13:55 +0100

    Update(ArtifactContent): Add artifactType

--- src/main/java/io/apicurio/registry/systemtests/client/ArtifactContent.java
+++ src/main/java/io/apicurio/registry/systemtests/client/ArtifactContent.java
@@ -1,6 +1,15 @@
 package io.apicurio.registry.systemtests.client;
 
 public final class ArtifactContent {
-    public static final String DEFAULT_AVRO = "{\"name\":\"price\",\"namespace\":\"com.example\",\"type\":\"record\"," +
-            "\"fields\":[{\"name\":\"symbol\",\"type\":\"string\"},{\"name\":\"price\",\"type\":\"string\"}]}";
+    public static final String DEFAULT_AVRO = "{" +
+            "\"artifactType\":\"AVRO\"," +
+            "\"name\":\"price\"," +
+            "\"namespace\":\"com.example\"," +
+            "\"type\":\"record\"," +
+            "\"fields\":" + "[" +
+            "{\"name\":\"symbol\"," + "\"type\":\"string\"}" +
+            "," +
+            "{\"name\":\"price\"," + "\"type\":\"string\"}" +
+            "]" +
+            "}";
 }
