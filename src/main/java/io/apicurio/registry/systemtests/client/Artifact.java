a764a18dbee6dc8eec47cc5c378f32ea674f59f0
Radim Kubis <rkubis@redhat.com>
2024-12-02 16:55:54 +0100

    Update(Artifact): Add owner, use Lombok

--- src/main/java/io/apicurio/registry/systemtests/client/Artifact.java
+++ src/main/java/io/apicurio/registry/systemtests/client/Artifact.java
@@ -1,11 +1,15 @@
 package io.apicurio.registry.systemtests.client;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+@Setter
+@Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Artifact {
     private String id;
@@ -18,6 +22,7 @@ public class Artifact {
     private String modifiedBy;
     private String groupId;
     private String description;
+    private String owner;
     private List<String> labels;
     private String version;
     private Long globalId;
@@ -25,131 +30,4 @@ public class Artifact {
     private Long contentId;
     private List<ArtifactReference> references;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Date getCreatedOn() {
-        return createdOn;
-    }
-
-    public void setCreatedOn(Date createdOn) {
-        this.createdOn = createdOn;
-    }
-
-    public String getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public ArtifactState getState() {
-        return state;
-    }
-
-    public void setState(ArtifactState state) {
-        this.state = state;
-    }
-
-    public Date getModifiedOn() {
-        return modifiedOn;
-    }
-
-    public void setModifiedOn(Date modifiedOn) {
-        this.modifiedOn = modifiedOn;
-    }
-
-    public String getModifiedBy() {
-        return modifiedBy;
-    }
-
-    public void setModifiedBy(String modifiedBy) {
-        this.modifiedBy = modifiedBy;
-    }
-
-    public String getGroupId() {
-        return groupId;
-    }
-
-    public void setGroupId(String groupId) {
-        this.groupId = groupId;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public List<String> getLabels() {
-        return labels;
-    }
-
-    public void setLabels(List<String> labels) {
-        this.labels = labels;
-    }
-
-    public String getVersion() {
-        return version;
-    }
-
-    public void setVersion(String version) {
-        this.version = version;
-    }
-
-    public Long getGlobalId() {
-        return globalId;
-    }
-
-    public void setGlobalId(Long globalId) {
-        this.globalId = globalId;
-    }
-
-    public Map<String, String> getProperties() {
-        return properties;
-    }
-
-    public void setProperties(Map<String, String> properties) {
-        this.properties = properties;
-    }
-
-    public Long getContentId() {
-        return contentId;
-    }
-
-    public void setContentId(Long contentId) {
-        this.contentId = contentId;
-    }
-
-    public List<ArtifactReference> getReferences() {
-        return references;
-    }
-
-    public void setReferences(List<ArtifactReference> references) {
-        this.references = references;
-    }
 }
