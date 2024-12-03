a7ae7e0574f8d275689e99382831de24c5985a9d
Radim Kubis <rkubis@redhat.com>
2024-12-03 14:57:54 +0100

    New(ContentType): Class with artifact content types

---
+++ src/main/java/io/apicurio/registry/systemtests/client/ContentType.java
@@ -0,0 +1,11 @@
+package io.apicurio.registry.systemtests.client;
+
+public class ContentType {
+
+    public static final String APPLICATION_JSON = "application/json";
+    public static final String APPLICATION_YAML = "application/x-yaml";
+    public static final String APPLICATION_XML = "application/xml";
+    public static final String APPLICATION_PROTOBUF = "application/x-protobuf";
+    public static final String APPLICATION_GRAPHQL = "application/graphql";
+
+}
