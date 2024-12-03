3f3f645908c8e5540effd717861353759e6d068a
Radim Kubis <rkubis@redhat.com>
2024-12-03 15:33:49 +0100

    New(pom.xml): Bundle RapiDAST debug profile

--- src/test/java/io/apicurio/registry/systemtests/rapidast/RapidastTests.java
+++ src/test/java/io/apicurio/registry/systemtests/rapidast/RapidastTests.java
@@ -19,6 +19,7 @@ public abstract class RapidastTests extends TestBase {
     @Tag("unauthenticated")
     @Tag("v2")
     @Tag("v3")
+    @Tag("debug")
     public void testRapidastRegistryV2Unauthenticated() throws InterruptedException {
         // Get path to config file
         String configFilePath = RapidastUtils.getRapidastFilePath("registry_v2_unauthenticated.yaml");
