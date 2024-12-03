b5339f16f59c3f029b4b237780d12ee4c7f8ae38
Radim Kubis <rkubis@redhat.com>
2024-12-03 14:55:27 +0100

    Update(ArtifactContent): New default AVRO

--- src/main/java/io/apicurio/registry/systemtests/client/ArtifactContent.java
+++ src/main/java/io/apicurio/registry/systemtests/client/ArtifactContent.java
@@ -1,18 +1,13 @@
 package io.apicurio.registry.systemtests.client;
 
-import io.apicurio.registry.systemtests.framework.Constants;
-
 public final class ArtifactContent {
-    public static final String DEFAULT_AVRO = "{" +
-            "\"artifactType\":\"AVRO\"," +
-            "\"artifactId\":\"" + Constants.AVRO_ARTIFACT_ID_PLACEHOLDER + "\"," +
-            "\"name\":\"price\"," +
-            "\"namespace\":\"com.example\"," +
-            "\"type\":\"record\"," +
-            "\"fields\":" + "[" +
-            "{\"name\":\"symbol\"," + "\"type\":\"string\"}" +
-            "," +
-            "{\"name\":\"price\"," + "\"type\":\"string\"}" +
+    public static final String DEFAULT_AVRO = "\"{" +
+            "\\\"name\\\":\\\"price\\\"" + "," +
+            "\\\"namespace\\\":\\\"com.example\\\"" + "," +
+            "\\\"type\\\":\\\"record\\\"" + "," +
+            "\\\"fields\\\": [" +
+                "{\\\"name\\\":\\\"symbol\\\", \\\"type\\\":\\\"string\\\"}" + "," +
+                "{\\\"name\\\":\\\"price\\\", \\\"type\\\":\\\"string\\\"}" +
             "]" +
-            "}";
+            "}\"";
 }
