a67e5405e5d084607068d17740b13a85b7828772
Radim Kubis <rkubis@redhat.com>
2024-12-04 14:02:03 +0100

    New(ArtifactContent): Non-escaped version of artifact content

--- src/main/java/io/apicurio/registry/systemtests/client/ArtifactContent.java
+++ src/main/java/io/apicurio/registry/systemtests/client/ArtifactContent.java
@@ -2,12 +2,21 @@ package io.apicurio.registry.systemtests.client;
 
 public final class ArtifactContent {
     public static final String DEFAULT_AVRO = "\"{" +
-            "\\\"name\\\":\\\"price\\\"" + "," +
-            "\\\"namespace\\\":\\\"com.example\\\"" + "," +
-            "\\\"type\\\":\\\"record\\\"" + "," +
-            "\\\"fields\\\": [" +
-                "{\\\"name\\\":\\\"symbol\\\", \\\"type\\\":\\\"string\\\"}" + "," +
-                "{\\\"name\\\":\\\"price\\\", \\\"type\\\":\\\"string\\\"}" +
-            "]" +
-            "}\"";
+        "\\\"name\\\":\\\"price\\\"" + "," +
+        "\\\"namespace\\\":\\\"com.example\\\"" + "," +
+        "\\\"type\\\":\\\"record\\\"" + "," +
+        "\\\"fields\\\": [" +
+            "{\\\"name\\\":\\\"symbol\\\", \\\"type\\\":\\\"string\\\"}" + "," +
+            "{\\\"name\\\":\\\"price\\\", \\\"type\\\":\\\"string\\\"}" +
+        "]" +
+    "}\"";
+    public static final String DEFAULT_AVRO_PLAIN = "\"{" +
+        "\"name\":\"price\"" + "," +
+        "\"namespace\":\"com.example\"" + "," +
+        "\"type\":\"record\"" + "," +
+        "\"fields\": [" +
+            "{\"name\":\"symbol\", \"type\":\"string\"}" + "," +
+            "{\"name\":\"price\", \"type\":\"string\"}" +
+        "]" +
+    "}\"";
 }
