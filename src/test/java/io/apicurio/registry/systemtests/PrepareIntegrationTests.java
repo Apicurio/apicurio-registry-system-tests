a984e4a88d86de08c76a8161817f0d579db6a75e
Radim Kubis <rkubis@redhat.com>
2024-12-12 20:56:20 +0100

    New(PrepareIntegrationTests): Useful tests for Registry3

--- src/test/java/io/apicurio/registry/systemtests/PrepareIntegrationTests.java
+++ src/test/java/io/apicurio/registry/systemtests/PrepareIntegrationTests.java
@@ -2,19 +2,34 @@ package io.apicurio.registry.systemtests;
 
 import io.apicur.registry.v1.ApicurioRegistry3;
 import io.apicur.registry.v1.ApicurioRegistry3Builder;
+import io.apicurio.registry.systemtests.framework.ApicurioRegistryUtils;
+import io.apicurio.registry.systemtests.framework.Base64Utils;
+import io.apicurio.registry.systemtests.framework.Certificate;
+import io.apicurio.registry.systemtests.framework.CertificateUtils;
 import io.apicurio.registry.systemtests.framework.Constants;
 import io.apicurio.registry.systemtests.framework.DatabaseUtils;
 import io.apicurio.registry.systemtests.framework.Environment;
 import io.apicurio.registry.systemtests.framework.KeycloakUtils;
 import io.apicurio.registry.systemtests.framework.LoggerUtils;
 import io.apicurio.registry.systemtests.operator.OperatorManager;
+import io.apicurio.registry.systemtests.operator.types.ApicurioRegistryOLMOperatorType;
 import io.apicurio.registry.systemtests.operator.types.KeycloakOLMOperatorType;
 import io.apicurio.registry.systemtests.platform.Kubernetes;
+import io.apicurio.registry.systemtests.registryinfra.ResourceManager;
+import io.apicurio.registry.systemtests.registryinfra.resources.PersistenceKind;
+import io.fabric8.kubernetes.api.model.Secret;
+import io.fabric8.kubernetes.api.model.SecretBuilder;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 
 import java.io.IOException;
+import java.nio.file.Path;
 import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class PrepareIntegrationTests {
     protected static Logger LOGGER = LoggerUtils.getLogger();
@@ -87,5 +102,63 @@ public class PrepareIntegrationTests {
                     .endStudioUi()
                 .endSpec()
                 .build();
+    }
+
+    @Test
+    public void installOLMClusterWideRegistryOperator() throws InterruptedException {
+        ApicurioRegistryOLMOperatorType operator = new ApicurioRegistryOLMOperatorType(Environment.CATALOG_IMAGE, true);
+
+        operator.install();
+    }
+
+    @Test
+    public void prepareTruststore() throws InterruptedException {
+        Secret routerCertsDefaultSecret = Kubernetes.getSecret("openshift-config-managed", Constants.ROUTER_CERTS);
+        String clusterBaseUrl = Objects.requireNonNull(
+                Kubernetes.getRouteHost("openshift-console", "console")
+        ).replace("console-openshift-console.", "");
+        ArrayList<Certificate> certificates = CertificateUtils.readCertificates(
+                Base64Utils.decode(routerCertsDefaultSecret.getData().get(clusterBaseUrl))
+        );
+        Secret newSecret = new SecretBuilder()
+                .withNewMetadata()
+                    .withName(routerCertsDefaultSecret.getMetadata().getName())
+                    .withNamespace(Environment.NAMESPACE)
+                .endMetadata()
+                .withType("Opaque")
+                .withData(new HashMap<>() {{
+                    put(Constants.TRUSTSTORE_DATA_NAME, Base64Utils.encode(CertificateUtils.getCertificates(certificates)));
+                }})
+                .build();
+
+        Kubernetes.createSecret(Environment.NAMESPACE, newSecret);
+
+        String timestamp = String.valueOf(Instant.now().getEpochSecond());
+        String caCertSecretValue = CertificateUtils.decodeBase64Secret(Environment.NAMESPACE, Constants.ROUTER_CERTS, Constants.TRUSTSTORE_DATA_NAME);
+        Path caPath = Environment.getTmpPath("tls-" + timestamp + ".crt");
+
+        CertificateUtils.writeToFile(caCertSecretValue, caPath);
+
+        Path truststorePath = Environment.getTmpPath("truststore-" + timestamp + ".p12");
+
+        CertificateUtils.runTruststoreCmd(truststorePath, "password", caPath);
+
+        Secret secret = new SecretBuilder()
+                .withNewMetadata()
+                    .withName(Constants.TRUSTSTORE_SECRET_NAME)
+                    .withNamespace(Environment.NAMESPACE)
+                .endMetadata()
+                .addToData(new HashMap<>() {{
+                    put(Constants.TRUSTSTORE_DATA_NAME, Base64Utils.encode(truststorePath));
+                }})
+                .build();
+
+        Kubernetes.createSecret(Environment.NAMESPACE, secret);
+    }
+
+    @Test
+    public void prepareRegistry3SqlNoIAM() throws InterruptedException {
+        // Deploy and get registry
+        ApicurioRegistry3 apicurioRegistry3 = ApicurioRegistryUtils.deployDefaultApicurioRegistrySql(false);
     }
 }
