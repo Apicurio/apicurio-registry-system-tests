17c311c1706d88209965cbdc26ebcbae1703c9dc
Radim Kubis <rkubis@redhat.com>
2024-12-18 08:13:58 +0100

    Update(Environment/Constants): Beta suffix for operator

--- src/main/java/io/apicurio/registry/systemtests/framework/Environment.java
+++ src/main/java/io/apicurio/registry/systemtests/framework/Environment.java
@@ -42,7 +42,7 @@ public final class Environment {
     public static final String KAFKA_PACKAGE_DEFAULT = "amq-streams"; // Default from catalog
     public static final String POSTGRESQL_VERSION_DEFAULT = "17";
     public static final String REGISTRY_CHANNEL_DEFAULT = "3.x";
-    public static final String REGISTRY_CSV_DEFAULT = "apicurio-registry-3.v3.0.6";
+    public static final String REGISTRY_CSV_DEFAULT = "apicurio-registry-3.v3.0.6-beta";
     public static final String REGISTRY_BUNDLE_DEFAULT =
             "https://raw.githubusercontent.com/Apicurio/apicurio-registry-operator/main/install/" +
                     "apicurio-registry-operator-1.0.0-v2.0.0.final.yaml";
