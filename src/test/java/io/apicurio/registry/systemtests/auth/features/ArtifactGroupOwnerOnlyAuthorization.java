9e7633497ea9dc3ccd20096f6223c261677f43cd
Radim Kubis <rkubis@redhat.com>
2024-12-18 13:15:58 +0100

    Fix(ArtifactGroupOwnerOnlyAuthorization): Environment variables

--- src/test/java/io/apicurio/registry/systemtests/auth/features/ArtifactGroupOwnerOnlyAuthorization.java
+++ src/test/java/io/apicurio/registry/systemtests/auth/features/ArtifactGroupOwnerOnlyAuthorization.java
@@ -67,7 +67,7 @@ public class ArtifactGroupOwnerOnlyAuthorization {
         // ENABLE ARTIFACT OWNER ONLY AUTHORIZATION THAT IS REQUIRED FOR ARTIFACT GROUP OWNER ONLY AUTHORIZATION
         // Set environment variable REGISTRY_AUTH_OBAC_ENABLED of deployment to true
         ApicurioRegistryUtils.createOrReplaceEnvVar(apicurioRegistry, new Env() {{
-            setName("APICURIO_AUTH_OWNER_ONLY_AUTHORIZATION");
+            setName("APICURIO_AUTH_OWNER-ONLY-AUTHORIZATION");
             setValue("true");
         }});
         // Wait for API availability
@@ -119,7 +119,7 @@ public class ArtifactGroupOwnerOnlyAuthorization {
         // --- enable feature
         // Set environment variable REGISTRY_AUTH_OBAC_LIMIT_GROUP_ACCESS of deployment to true
         ApicurioRegistryUtils.createOrReplaceEnvVar(apicurioRegistry, new Env() {{
-            setName("APICURIO_AUTH_OBAC_LIMIT_GROUP_ACCESS");
+            setName("APICURIO_AUTH_OWNER-ONLY-AUTHORIZATION_LIMIT-GROUP-ACCESS");
             setValue("true");
         }});
         // Wait for API availability
@@ -165,7 +165,7 @@ public class ArtifactGroupOwnerOnlyAuthorization {
         // DISABLE ARTIFACT GROUP OWNER ONLY AUTHORIZATION AND TEST IT
         // Set environment variable REGISTRY_AUTH_OBAC_LIMIT_GROUP_ACCESS of deployment to false
         ApicurioRegistryUtils.createOrReplaceEnvVar(apicurioRegistry, new Env() {{
-            setName("APICURIO_AUTH_OBAC_LIMIT_GROUP_ACCESS");
+            setName("APICURIO_AUTH_OWNER-ONLY-AUTHORIZATION_LIMIT-GROUP-ACCESS");
             setValue("false");
         }});
         // Define artifact ID for the owner part
