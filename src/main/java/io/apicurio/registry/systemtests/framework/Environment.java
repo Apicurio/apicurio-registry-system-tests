e5c91ff9173b3414d6f6ebde1fad5c549cc08d77
Radim Kubis <rkubis@redhat.com>
2025-03-06 08:37:44 +0100

    Update(Environment): Operator deployment name

--- src/main/java/io/apicurio/registry/systemtests/framework/Environment.java
+++ src/main/java/io/apicurio/registry/systemtests/framework/Environment.java
@@ -46,9 +46,9 @@ public final class Environment {
     public static final String REGISTRY_BUNDLE_DEFAULT =
             "https://raw.githubusercontent.com/Apicurio/apicurio-registry-operator/main/install/" +
                     "apicurio-registry-operator-1.0.0-v2.0.0.final.yaml";
-    public static final String REGISTRY_OPERATOR_DEPLOYMENT_NAME_DEFAULT = "apicurio-registry-operator-v2.6.12-r1";
+    public static final String REGISTRY_OPERATOR_DEPLOYMENT_NAME_DEFAULT = "apicurio-registry-operator-v3.0.6-beta.r1";
     public static final String REGISTRY_OPERATOR_DEPLOYMENT_NAME_UPGRADE_DEFAULT
-            = "apicurio-registry-operator-v2.6.12-r1";
+            = "apicurio-registry-operator-v3.0.6-beta.r1";
     public static final String REGISTRY_PACKAGE_DEFAULT = "apicurio-registry-3"; // Default from catalog
     public static final String SSO_PACKAGE_DEFAULT = "rhbk-operator"; // Default from catalog
     public static final String SSO_CHANNEL_DEFAULT = "stable-v26";
