39e9d8e8513849e18c5c23827867351e45df80a5
Radim Kubis <rkubis@redhat.com>
2025-06-16 09:45:56 +0200

    Update(Environment): New CSV and deployment name

--- src/main/java/io/apicurio/registry/systemtests/framework/Environment.java
+++ src/main/java/io/apicurio/registry/systemtests/framework/Environment.java
@@ -42,13 +42,13 @@ public final class Environment {
     public static final String KAFKA_PACKAGE_DEFAULT = "amq-streams"; // Default from catalog
     public static final String POSTGRESQL_VERSION_DEFAULT = "17";
     public static final String REGISTRY_CHANNEL_DEFAULT = "3.x";
-    public static final String REGISTRY_CSV_DEFAULT = "apicurio-registry-3.v3.0.7-r1";
+    public static final String REGISTRY_CSV_DEFAULT = "apicurio-registry-3.v3.0.9-r1";
     public static final String REGISTRY_BUNDLE_DEFAULT =
             "https://raw.githubusercontent.com/Apicurio/apicurio-registry-operator/main/install/" +
                     "apicurio-registry-operator-1.0.0-v2.0.0.final.yaml";
-    public static final String REGISTRY_OPERATOR_DEPLOYMENT_NAME_DEFAULT = "apicurio-registry-operator-v3.0.6-beta.r1";
+    public static final String REGISTRY_OPERATOR_DEPLOYMENT_NAME_DEFAULT = "apicurio-registry-operator-v3.0.9-r1";
     public static final String REGISTRY_OPERATOR_DEPLOYMENT_NAME_UPGRADE_DEFAULT
-            = "apicurio-registry-operator-v3.0.6-beta.r1";
+            = "apicurio-registry-operator-v3.0.9-r1";
     public static final String REGISTRY_PACKAGE_DEFAULT = "apicurio-registry-3"; // Default from catalog
     public static final String SSO_PACKAGE_DEFAULT = "rhbk-operator"; // Default from catalog
     public static final String SSO_CHANNEL_DEFAULT = "stable-v26";
